082ab6e0da03c81784ea0cdadef56d901eea5e9f
Daniel J. Hofmann <daniel@trvx.org>
2012-05-05 19:50:43 +0200

    function initializer

--- initializer.cpp
+++ initializer.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+void flist(initializer_list<int>);
+
 int main() {
   const vector<int> *w = new vector<int>{1, 2, 3};
 
@@ -10,4 +13,11 @@ int main() {
     cout << *it << endl;
 
   delete w;
+
+  flist({4, 5, 6});
+}
+
+void flist(initializer_list<int> list) {
+  for(const int *p = list.begin(); p != list.end(); ++p)
+    cout << *p << endl;
 }
