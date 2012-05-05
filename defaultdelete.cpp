2b837411a3c154bc0fdf707a6b13e5a7a2f951ca
Daniel J. Hofmann <daniel@trvx.org>
2012-05-06 00:13:25 +0200

    default delete

---
+++ defaultdelete.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+class A {
+  public:
+    A() = default; /* explicit default constructor */
+    A& operator=(const A&) = delete;
+    A(const A&) = delete;
+
+    void func(double);
+    void func(int) = delete;
+};
+
+int main() {
+  A t;
+
+  /* compile time error t.func(3); */
+}
