1d8fb815993b314d33b469e361d911a6e6822bff
Daniel J. Hofmann <daniel@trvx.org>
2012-05-06 01:45:42 +0200

    static assert

---
+++ staticassert.cpp
@@ -0,0 +1,5 @@
+int main() {
+  static_assert(sizeof(char) == 1, "char assert went wrong");
+  static_assert(sizeof(int) == 4, "int assert went wrong");
+  static_assert(sizeof(double) == 8, "double assert went wrong");
+}
