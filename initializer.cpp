02c018612dfd3341a07d2fdfce240770fc750002
Daniel J. Hofmann <daniel@trvx.org>
2012-05-05 15:53:44 +0200

    initialization lists

---
+++ initializer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  const vector<int> *w = new vector<int>{1, 2, 3};
+
+  for(vector<int>::const_iterator it = w->begin(); it != w->end(); ++it)
+    cout << *it << endl;
+}
