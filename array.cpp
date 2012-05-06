25dc5ab1fc0ac0fbc904fa5f3e050167507d6cda
Daniel J. Hofmann <daniel@trvx.org>
2012-05-06 05:44:25 +0200

    array

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+  array<int, 3> a = {1, 2, 3};
+
+  cout << "first " << a.front() << endl;
+  cout << "last " << a.back() << endl;
+
+  for(auto it = a.cbegin(); it != a.cend(); ++it)
+    cout << *it << endl;
+}
