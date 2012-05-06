a451edfc62c92c92570e1cd85f7e80e16a77e5cb
Daniel J. Hofmann <daniel@trvx.org>
2012-05-06 16:22:56 +0200

    future

---
+++ future.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <future>
+
+using namespace std;
+
+/* compile wirh -pthread */
+
+int cfunc() {
+  return 42; /* do your async calc here */
+}
+
+int main() {
+  /* launch::async = asynchronous eval., launch::deferred = lazy eval. */
+  future<int> result = async(launch::async, cfunc);
+
+  /* do other stuff, while the future result is calculated */
+
+  cout << "we need the result now, and it is: " << result.get() << endl;
+}
