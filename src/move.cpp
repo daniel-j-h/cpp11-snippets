37b9eb32e4604b7134138eba075d4e59f7969877
Daniel J. Hofmann <daniel@trvx.org>
2012-08-27 03:28:17 +0200

    move semantics

---
+++ src/move.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Fruit {
+  public:
+    Fruit(string& label_) : label(label_) { }
+    Fruit(string&& label_) : label(move(label_)) { }
+
+    /* copy constuctor*/
+    Fruit(const Fruit& other) : label(other.label) {
+      cout << "copy constructor" << endl;
+    }
+
+    /* copy assign */
+    Fruit& operator=(Fruit& other) {
+      cout << "copy assign" << endl;
+
+      label = other.label;
+      return *this;
+    }
+
+    /* move constructor */
+    Fruit(Fruit&& other) : label(move(other.label)) {
+      cout << "move constructor" << endl;
+    }
+
+    /* move assign */
+    Fruit& operator=(Fruit&& other) {
+      cout << "move assign" << endl;
+
+      label = move(other.label);
+      return *this;
+    }
+
+    string getLabel() { return label; }
+
+  private:
+    string label;
+};
+
+
+int main() {
+  Fruit firstFruit("extra juicy");
+
+  /* copy */
+  Fruit secondFruit(firstFruit);
+  /* move */
+  Fruit thirdFruit(move(firstFruit));
+
+  /* copy */
+  secondFruit = thirdFruit;
+  /* move */
+  firstFruit = move(thirdFruit);
+}
